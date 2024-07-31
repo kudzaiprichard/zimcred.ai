d692170357d019c8983d6168d4e023fb6eb247fa
Kudzai P Matizirofa <kudzaiprichard@gmail.com>
2024-07-31 15:20:18 +0200

    Initial Commit.

---
+++ zimcredai_backend/src/main/java/com/intela/zimcredai/ZimcredaiApplication.java
@@ -0,0 +1,13 @@
+package com.intela.zimcredai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZimcredaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZimcredaiApplication.class, args);
+	}
+
+}
---
+++ zimcredai_backend/src/test/java/com/intela/zimcredai/ZimcredaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intela.zimcredai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZimcredaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
